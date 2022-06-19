063d55612b6ab795a7f7b03c632faae09708066c
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-06-19 16:34:26 +0430

    add header component

---
+++ HWs/17/advanced-todo/src/icons/index.js
@@ -0,0 +1,9 @@
+import {TbListNumbers} from "react-icons/tb"
+import {FaFilter} from "react-icons/fa"
+import {BsPlusSquareFill} from "react-icons/bs"
+import { MdDelete, MdModeEdit } from "react-icons/md"
+import {AiFillEye} from "react-icons/ai"
+
+
+
+export {TbListNumbers, FaFilter, BsPlusSquareFill, MdDelete, MdModeEdit, AiFillEye}
